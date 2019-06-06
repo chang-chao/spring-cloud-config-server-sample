ad1e4dd50f5dfd3c53acbe746b81a3a9b8d6d3aa
changchao <chao.chang@linecorp.com>
2019-06-06 18:27:35 +0900

    first commit

---
+++ src/test/java/me/changchao/spring/cloud/sample/springcloudconfigserversample/SpringCloudConfigServerSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package me.changchao.spring.cloud.sample.springcloudconfigserversample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConfigServerSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
