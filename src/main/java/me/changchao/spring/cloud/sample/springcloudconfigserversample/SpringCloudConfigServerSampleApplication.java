0a348dcffda64ce4c290f90a69eb0dabe97427cc
changchao <chao.chang@linecorp.com>
2019-06-12 13:02:46 +0900

    issue 604

--- src/main/java/me/changchao/spring/cloud/sample/springcloudconfigserversample/SpringCloudConfigServerSampleApplication.java
+++ src/main/java/me/changchao/spring/cloud/sample/springcloudconfigserversample/SpringCloudConfigServerSampleApplication.java
@@ -1,15 +1,22 @@
 package me.changchao.spring.cloud.sample.springcloudconfigserversample;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigServer
 public class SpringCloudConfigServerSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigServerSampleApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudConfigServerSampleApplication.class, args);
+  }
 
+  @Bean
+  Object beanUsingFooBar(@Value("${foo.bar}") String fooBar) {
+    System.out.println("fooBar=" + fooBar);
+    return new Object();
+  }
 }
